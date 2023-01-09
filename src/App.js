c50ef6c65c8d69b9f2946fd57fdc8dd73b786469
Ashgh89 <ghaneiash@gmail.com>
2023-01-09 12:49:24 +0100

    m

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   };
   return (
     <div className="App">
-      <button onClick={handleClick}>Just Reactt</button>
+      <button onClick={handleClick}>Just Reacttt</button>
     </div>
   );
 }
